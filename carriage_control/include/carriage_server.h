f8a030abeaad1bd34d17af0cc22ce84b8342ae46
Alexander Gornostaev <aleksgornostaev@gmail.com>
2019-08-06 19:05:14 +0300

    Simple standalone server implemented

--- carriage_control/include/carriage_server.h
+++ carriage_control/include/carriage_server.h
@@ -4,26 +4,43 @@
 //Including some useful stuff
 #include <stdlib.h>
 //Custom predefined action messages
-#include <carriageAction.h>
+#include <carriage_control/carriageAction.h>
 
 //включаем ROS
 #include <ros/ros.h>
 #include <actionlib/server/simple_action_server.h>
+#include <gazebo_msgs/GetModelState.h>
 #include <std_msgs/Bool.h>
 #include <std_msgs/Int32.h>
 #include <math.h>
 
 class Carriage_Server{
     private:
-        ros::NodeHandle n;
+        struct Cell{
+            int32_t x;
+            int32_t y;
+        } cell;
+        struct Position{
+            double x;
+            double y;
+        } position;
+        
+        ros::NodeHandle nh;
         actionlib::SimpleActionServer<carriage_control::carriageAction> ac;
-        std::string action_name_;
+        const std::string action_name;
+        const std::string robot_name;
         // create messages that are used to published feedback/result
         carriage_control::carriageFeedback feedback_;
         carriage_control::carriageResult result_;
-
+        //client to obtain position of robot from gazebo topics
+        ros::ServiceClient model_states_client;
+        gazebo_msgs::GetModelState gazebo_srv;
+        void getModelPosition();
+        void getCell();
+        void initializeVars();
     public:
-        Carriage_Server();
-
+        Carriage_Server(std::string name, std::string robot_name);
+        ~Carriage_Server();
+        void showCell();
 };
 #endif
